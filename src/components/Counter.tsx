a39ea6dfd0f85e4db1b182f604d239216b1727cf
claire froelich <claire.froelich@gmail.com>
2020-08-18 16:38:41 -0700

    add counter

---
+++ src/components/Counter.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return <button onClick={increment}>Clicks: {count}</button>;
+}
+
+export default Counter;
