376bf5c21e3f68f31f578bb8b95aedefbaf4b71b
claire froelich <claire.froelich@gmail.com>
2020-08-19 07:56:42 -0700

    should fail

--- src/___tests___/sum.test.ts
+++ src/___tests___/sum.test.ts
@@ -8,4 +8,7 @@ describe("correctly sums numbers", () => {
   it("adds 1 + (-2) to equal -1", () => {
     expect(sum(1, -2)).toBe(-1);
   });
+  it("adds 1 + (-2) to equal -1", () => {
+    expect(sum(1, "a")).toBe(-1);
+  });
 });
