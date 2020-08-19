8c5348fe6dbfa2f7ac13c65e18f2ba7f546c8d71
claire froelich <claire.froelich@gmail.com>
2020-08-18 17:32:05 -0700

    purposefully fail test

--- src/___tests___/sum.test.ts
+++ src/___tests___/sum.test.ts
@@ -4,7 +4,11 @@ describe("correctly sums numbers", () => {
   it("adds 1 + 2 to equal 3", () => {
     expect(sum(1, 2)).toBe(3);
   });
+
   it("adds 1 + (-2) to equal -1", () => {
     expect(sum(1, -2)).toBe(-1);
+  });
+  it("fails", () => {
+    expect(sum(1, -2)).toBe(0);
   });
 });
